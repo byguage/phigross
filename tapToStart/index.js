0c4e8c9f982125f9595b9c63ad000850f1c78358
byguage <110472388+byguage@users.noreply.github.com>
2023-05-13 20:07:51 +0800

    Add files via upload

--- tapToStart/index.js
+++ tapToStart/index.js
@@ -1,14 +1,9 @@
+import './style.css';
+import tapToStart_mp3 from './TapToStart.mp3';
+
 window.addEventListener('DOMContentLoaded', () => {
-	fetch('https://api.github.com/repos/Yuameshi/PhiCommunity/commits?per_page=1').then(
-		(response) => {
-			response.json().then((data) => {
-				document.querySelector('#ver').innerText = data[0].sha.slice(
-					0,
-					7
-				);
-			});
-		}
-	);
+	document.querySelector('#ver').innerText = $VERSION;
+
 	try {
 		document.querySelector('#device').innerText =
 			'Platform: ' +
@@ -21,29 +16,29 @@ window.addEventListener('DOMContentLoaded', () => {
 			navigator.userAgent.slice(navigator.userAgent.lastIndexOf(' '));
 	}
 	document.querySelector('#device').title = navigator.userAgent;
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', './TapToStart.mp3', true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function () {
-		const actx = new (window.AudioContext ||
-			window.webkitAudioContext ||
-			window.mozAudioContext ||
-			window.msAudioContext)();
-		actx.decodeAudioData(this.response, function (buffer) {
-			let source = actx.createBufferSource();
-			source.buffer = buffer;
-			source.loop = true;
-			source.connect(actx.destination);
-			source.start(0);
+	const actx = new (window.AudioContext ||
+		window.webkitAudioContext ||
+		window.mozAudioContext ||
+		window.msAudioContext)();
+	const abortController = new AbortController();
+	fetch(tapToStart_mp3, abortController.signal)
+		.then((res) => res.arrayBuffer())
+		.then((arrayBuffer) => {
+			actx.decodeAudioData(arrayBuffer, function (buffer) {
+				var source = actx.createBufferSource();
+				source.buffer = buffer;
+				source.loop = true;
+				source.connect(actx.destination);
+				source.start(0);
+			});
 		});
-	};
-	xhr.send();
 	document.body.addEventListener('click', () => {
 		console.log('Clicked! Redirecting to MainPage');
 		var fadeInElem = document.createElement('div');
 		fadeInElem.classList.add('fadeIn');
 		document.body.appendChild(fadeInElem);
 		setTimeout(() => {
+			actx == undefined ? abortController.abort() : actx.close();
 			if (window.localStorage.length == 0) {
 				location.href = '../settings/index.html';
 			} else {
@@ -52,3 +47,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 510);
 	});
 });
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('../service-worker.js')
+			.then((registration) => {
+				console.log('SW registered: ', registration);
+			})
+			.catch((registrationError) => {
+				console.log('SW registration failed: ', registrationError);
+			});
+	});
+}
